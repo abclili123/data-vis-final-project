bfa9f15e608769b8867ce0d239af97e06b2c39b0
abclili123 <lilianaseoror@gmail.com>
2025-04-22 11:41:46 -0400

    more readable

--- src/graphs/list_countries.js
+++ src/graphs/list_countries.js
@@ -19,6 +19,7 @@ const ListCountries = ({ data, selectedYear }) => {
   const min = d3.min(valuesWithNumbers, d => d.value);
   const max = d3.max(valuesWithNumbers, d => d.value);
 
+  const fontSizeScale = d3.scaleSqrt().domain([min, max]).range([12, 24]);
   const opacityScale = d3.scaleLinear().domain([min, max]).range([0.4, 1]);
 
   // Sort descending by value
@@ -36,12 +37,13 @@ const ListCountries = ({ data, selectedYear }) => {
       fontFamily: 'sans-serif'
     }}>
       {sorted.map((d, i) => {
+        const fontSize = d.value ? fontSizeScale(d.value) : fontSizeScale(min);
         const opacity = d.value ? opacityScale(d.value) : opacityScale(min);
         return (
           <div
             key={i}
             style={{
-              fontSize: `24px`,
+              fontSize: `${fontSize}px`,
               opacity: opacity
             }}
           >
